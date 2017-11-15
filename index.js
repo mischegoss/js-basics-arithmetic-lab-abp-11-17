57268399fee86884c3bc7901cd49709b0e86ffaf
mischegoss <mischegoss@gmail.com>
2017-11-15 02:10:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ newID = 1000000000 + oldID;
 ageIsValid = Number.isInteger(currentAge);
 randomNumber = (Math.random() * 19) + 1;
 randomInteger = (Math.floor(randomNumber));
+randomUserID = 1000000000 + randomInteger;
